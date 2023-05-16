29eb34f343d33f3a9a47fed7da19e0a89aa7447b
Avi <avibanerjee2000@gmail.com>
2023-05-16 20:46:32 +0530

    use function and if to modify

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
 // Dom manipulation
 
-let skills = document.querySelector('#skills');
-skills.setAttribute("href", " ");
 
+function updateSkillsLink() {
+    let skills = document.getElementById('skills');
+    skills.setAttribute('href', '');
+  }
+  
+  function redirectToSkillPage(event) {
+    event.preventDefault();
+    window.location.href = 'skill.html';
+  }
+  
+  function updateDescriptionText() {
+    let description = document.getElementById('js');
+    description.innerText = "JavaScript: JavaScript's DOM manipulation capabilities are indispensable for web development. With methods like getElementById, querySelector, and appendChild, developers can dynamically modify HTML, CSS, and content. Whether it's updating text, adding elements, or altering styles, JavaScript empowers developers to create interactive and responsive web applications with ease.";
+  }
 
-var skl = document.querySelector('.skl');
-skl.addEventListener('click', function(e){
-    window.location.href = "skill.html";
-    console.log('clicked!')
-    e.preventDefault()
-})
+  
 
-
+  // Call shared functions on specific pages
+if (document.getElementById('skills')) {
+    updateSkillsLink();
+  }
+  
+  if (document.getElementById('skl')) {
+    var skl = document.getElementById('skl');
+    skl.addEventListener('click', redirectToSkillPage);
+  }
+  
+  if (document.getElementById('js')) {
+    updateDescriptionText();
+  }
+  
